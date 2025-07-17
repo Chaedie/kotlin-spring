5dbeed6753a625ee8d734410cce5def2e1f501ff
chaedongim <chaedong.im@cj.net>
2025-07-18 08:51:45 +0900

    refactor: 쿼리 DSL 적용

--- src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
@@ -3,13 +3,4 @@ package com.group.libraryapp.domain.user.loanhistory
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserLoanHistoryRepository : JpaRepository<UserLoanHistory, Long> {
-
-    fun findByBookNameAndStatus(bookName: String, status: UserLoanStatus): UserLoanHistory?
-
-    fun findByUserName(userName: String): List<UserLoanHistory>
-
-    fun findAllByStatus(status: UserLoanStatus): List<UserLoanHistory>
-
-    fun countByStatus(status: UserLoanStatus): Long
-
 }
